21e28c25975b31925b72350be5f511f0ed4aedc7
nidal_sentoke <nidalakhmedjanov@gmail.com>
2025-09-21 13:25:43 +0500

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next';
+import { Geist_Mono } from 'next/font/google';
+import '@/shared/globals.css';
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'Rackets Market',
+  description: 'Learn js education project',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={geistMono.variable}>{children}</body>
+    </html>
+  );
+}
